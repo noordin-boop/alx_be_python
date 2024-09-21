124b69ec4ef26bb92ac1217cc2995039e717955a
Noordin <noordinali156@gmail.com>
2024-09-21 15:58:52 +0300

    sum_of_numbers_using_forloop

---
+++ python_introduction/sum_of_numbers.py
@@ -0,0 +1,12 @@
+# Create a list of numbers
+numbers = [1, 5, 3, 9]
+
+# Initialize a variable to store the running sum
+total = 0
+
+# Use a for loop to iterate over the numbers list
+for number in numbers:
+    total += number  # Add the current number to total
+
+# Print the final total value
+print("The sum of all the numbers in the list is:", total)
